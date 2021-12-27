526318759749205f011ff86073561ba6fd9b828b
kgthompson1111 <kgthompson@null.net>
2021-12-27 14:34:54 -0700

    add overflow scroll to y axis of project pane

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ const projectManager = new projectFactory();
 // }
 
 // populate project list
-projectManager.projectList[0] = new projectManager.project('Example1');
-projectManager.projectList[1] = new projectManager.project('Example2');
-projectManager.projectList[2] = new projectManager.project('Example3');
+projectManager.projectList[0] = new projectManager.project('Today');
 
 projectManager.renderProjects();
